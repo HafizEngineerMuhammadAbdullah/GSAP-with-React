a5652f97858f9fa9fcc3cc67054f75a88d0ce508
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-24 11:34:21 -0700

    Complete Apple Macbook Pro

--- Apple-Macbook-Pro/src/App.jsx
+++ Apple-Macbook-Pro/src/App.jsx
@@ -1,25 +1,41 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react';
 import "./style.css"
-import { Environment, OrbitControls } from '@react-three/drei';
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei';
 import MacContainer from './MacContainer';
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 12, position: [0, -10, 220] }} >
-      <OrbitControls />
-      <Environment files={["/brown_photostudio_02_4k.exr"]} />
-      {/* <Environment preset="city" /> */}
-      {/* <Environment preset="sunset" />
-      <Environment preset="warehouse" />
-      <Environment preset="forest" />
-      <Environment preset="night" />
-      <Environment preset="dawn" />
-      <Environment preset="apartment" />
-      <Environment preset="park" />
-      <Environment preset="lobby" /> */}
-      <MacContainer />
-    </Canvas>
+    <div className='h-screen w-full'>
+      <nav className='navbar line flex gap-7 pt-10 pb-3 absolute top-0 left-1/2 -translate-x-1/2 font-["Helvetica_Now_Display"]'>
+        {["iPhone", "iPad", "Services", "ios", "Mac", "Products", "iPhone", "iPad", "Services", "ios", "Mac", "Products"].map((e, idx) => (
+          <a key={idx} href="" className='text-white font-[400] text-md capitalize'>
+            {e}
+          </a>
+        ))}
+      </nav>
+      <div className='absolute flex flex-col items-center text-white top-20 left-1/2 -translate-x-1/2'>
+        <h3 className='masked text-7xl font-[700] tracking-tighter'>Macbook pro.</h3>
+        <h5>Oh so pro !</h5>
+        <p className='text-center w-3/4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas pariatur laboriosam quibusdam.</p>
+      </div>
+      <Canvas camera={{ fov: 12, position: [0, -10, 220] }} >
+        <OrbitControls />
+        <Environment files={["/brown_photostudio_02_4k.exr"]} />
+        <Environment preset="city" />
+        <Environment preset="sunset" />
+        <Environment preset="warehouse" />
+        <Environment preset="forest" />
+        <Environment preset="night" />
+        <Environment preset="dawn" />
+        <Environment preset="apartment" />
+        <Environment preset="park" />
+        <Environment preset="lobby" />
+        <ScrollControls pages={3}>
+          <MacContainer />
+        </ScrollControls>
+      </Canvas>
+    </div>
   )
 }
 
--- Apple-Macbook-Pro/src/MacContainer.jsx
+++ Apple-Macbook-Pro/src/MacContainer.jsx
@@ -1,15 +1,35 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, useScroll, useTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
 import React from 'react'
+import * as THREE from "three";
 
 const MacContainer = () => {
 
-    let model = useGLTF("./mac.glb")
+    let model = useGLTF("./mac.glb");
+    let meshes = {};
+    let tex = useTexture("./red.jpg");
+    model.scene.traverse(e => {
+        meshes[e.name] = e;
+    });
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+    meshes.matte.material.map = tex;
+    meshes.matte.material.emissiveIntensity = 0;
+    meshes.matte.material.metalness = 0;
+    meshes.matte.material.roughness = 1;
+
+    let data = useScroll();
+
+    useFrame((state, delta) => {
+        // console.log(data.offset);
+        meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - (data.offset * 90))
+
+    })
     return (
         // <mesh>
         //     <boxGeometry />
         // </mesh>
 
-        <group position={[0,-10,20]}>
+        <group position={[0, -10, 20]}>
             <primitive object={model.scene} />
         </group>
     )
